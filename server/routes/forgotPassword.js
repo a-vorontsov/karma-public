0977422a0ce0b823e97de55280e7e27c9a8a3018
houssam966 <houssammahlous123@gmail.com>
2020-02-08 13:17:51 +0000

    initiate forgotPassword.js

---
+++ server/routes/forgotPassword.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+
+/**
+ * db needs to have the email set as unique
+ * user should also store resetPasswordToken as a string and resetPasswordExpires as a date 
+ * */
+
+
+router.get('/', (req, res) => {
+    res.send('forgot')
+})
+router.post('/',(req,res)=>{
+    //gets called when the person submits the forgot password button
+    const email =  req.body.email;
+    db.query('SELECT * FROM users WHERE email = $1', [email], (err, result) => {
+        const user = result.rows[0];
+        if (err) return err;
+        else if (!user) {
+            res.status(404).send({message:`There is no user with email ${email}`});
+            return res.redirect('/');
+        }
+        //generate the token using crypto
+
+        //store it in the user
+
+    })
+
+})
+
+module.exports = router;
