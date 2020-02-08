2e1d98740064dea328bcc339a82337462eee1868
houssam966 <houssammahlous123@gmail.com>
2020-02-08 13:31:16 +0000

    rename to forgot and install nodemailer

--- server/routes/forgot.js
+++ server/routes/forgot.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 /**
  * db needs to have the email set as unique
