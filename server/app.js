f4c430ca3b1fba93d36a8a91ef55df317c25e9b0
Daniel Almasi <almd@pm.me>
2020-02-10 11:52:31 +0000

    Set up view to ejs config
    Set up view to ejs config
    (for demoing usage only)

--- server/app.js
+++ server/app.js
@@ -8,10 +8,15 @@ const usersRoute = require('./routes/users');
 //Midleware
 app.use(express.json())
 
-app.use('/', require('./routes/index'));
+// app.use('/', require('./routes/index'));
 app.use('/users', usersRoute);
 
 //Connect to DB
+
+
+app.get("/", (req, res) => {
+  res.render('register.ejs');
+});
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT} ...`));
