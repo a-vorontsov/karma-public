910032b3ba096a940ee15bcf563f911b8a18adfb
Daniel Almasi <almd@pm.me>
2020-02-10 12:01:11 +0000

    Set up authentication permissions
    Set up authentication permissions
    so user can only view appropriate content

--- server/app.js
+++ server/app.js
@@ -5,6 +5,10 @@ require('dotenv/config')
 
 //Import Routes
 const usersRoute = require('./routes/users');
+
+//Set view engine (for demoing views)
+app.set('view-engine', 'ejs')
+
 //Midleware
 app.use(express.json())
 
@@ -14,9 +18,28 @@ app.use('/users', usersRoute);
 //Connect to DB
 
 
-app.get("/", (req, res) => {
-  res.render('register.ejs');
+// Render and direct to view based on user auth status
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
 });
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+
+// Status definition function
+function checkAuthenticated(req, res) {
+  return false;
+}
+
+function checkNotAuthenticated(req, res) {
+  return false;
+}
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT} ...`));
