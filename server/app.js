a8f326bd7ca42c9a5afc359469bbe3b509c64247
houssam966 <houssammahlous123@gmail.com>
2020-02-11 20:14:45 +0000

    initiate causes route

--- server/app.js
+++ server/app.js
@@ -5,13 +5,13 @@ require('dotenv/config')
 
 //Import Routes
 const usersRoute = require('./routes/users');
+const causesRoute = require('./routes/causes');
 //Midleware
 app.use(express.json())
 
 app.use('/', require('./routes/index'));
 app.use('/users', usersRoute);
-
-//Connect to DB
+app.use('/causes',causesRoute);
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT} ...`));
