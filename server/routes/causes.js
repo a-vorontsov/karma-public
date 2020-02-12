97763a327100b9166968ce1d73abd10a633280bf
houssam966 <houssammahlous123@gmail.com>
2020-02-12 20:17:21 +0000

    add causes/:id endpoint

--- server/routes/causes.js
+++ server/routes/causes.js
@@ -8,7 +8,23 @@ router.get('/', (req, res) => {
         if (err) {
             return err;
         }
-        res.status(200).send({users: result.rows})
+        res.status(200).send({causes: result.rows});
+    });
+});
+router.get('/:id',(req,res)=>{
+    let id = req.body.id;
+    if(!id){
+        return res.status(400).send("No id was specified in the body");
+    }
+    const query = `SELECT * FROM causes where id = '${id}'`;
+    db.query(query, [], (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        else if(result.rows.length == 0){
+            return res.status(400).send("There is no cause with that id")
+        }
+        res.status(200).send({cause: result.rows});
     });
 });
 
