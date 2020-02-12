290f9ad9fe13d2ea8b1ec3e9f3da320a61109b80
Daniel Almasi <almd@pm.me>
2020-02-12 23:49:58 +0000

    Update app security config
    Update app's security config with helmet and various other server security packages.
    Prevent server type detection

---
+++ server/routes/change-password.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../authentication/auth");
+
+router.get("/", auth.checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
+
+module.exports = router;
