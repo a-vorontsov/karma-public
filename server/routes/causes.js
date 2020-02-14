9d89ec476d0a2e8571da69fe6903ad50d2ff6444
houssam966 <houssammahlous123@gmail.com>
2020-02-14 11:23:19 +0000

    s

--- server/routes/causes.js
+++ server/routes/causes.js
@@ -12,29 +12,28 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/:id',(req,res)=>{
-    let id = req.params.id;
-    if(!id){
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    if (!id) {
         return res.status(400).send("No id was specified");
     }
     const query = `SELECT * FROM causes where id = '${id}'`;
     db.query(query, [], (err, result) => {
         if (err) {
             return res.status(500).send(err);
-        }
-        else if(result.rows.length == 0){
+        } else if (result.rows.length == 0) {
             return res.status(400).send("There is no cause with that id");
         }
         res.status(200).json(result.rows);
     });
 });
-//gets called when user selects causes
-router.post('/',(req,res)=>{
-    let causes = req.body.causes;
-    if(!id){
+// gets called when user selects causes
+router.post('/', (req, res) => {
+    const causes = req.body.causes;
+    if (!id) {
         return res.status(400).send("No causes were specified in the body");
     }
-    //update the db
+    // update the db
 });
 
 module.exports = router;
