5f1a57b7c9153451ff56a059af2aa06f71d3249c
Sten Laane <sten.laane@kcl.ac.uk>
2020-02-14 01:14:19 +0000

    Fix eslint formatting issues in event files

--- server/database/addressRepository.js
+++ server/database/addressRepository.js
@@ -2,7 +2,7 @@ const db = require("./connection");
 
 const insert = (address) => {
     const query = "INSERT INTO address(address_1, address_2, postcode, city, region, lat, long) VALUES ($1, $2, $3, $4, $5, $6, $7) " +
-        "RETURNING *";  // returns passed address with it's id set to corresponding id in database
+        "RETURNING *"; // returns passed address with it's id set to corresponding id in database
     const params = [address.address_1, address.address_2, address.postcode, address.city, address.region, address.lat, address.long];
     return db.query(query, params);
 };
