1465fc41d1aa497b0d47511f9421ec0602ddee09
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2020-02-16 13:23:31 +0000

    ESLint: fix formatting

--- app/Karma/App.js
+++ app/Karma/App.js
@@ -1,12 +1,11 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import SignUpScreen from './src/views/SignUpScreen';
-import WelcomeScreen from './src/views/WelcomeScreen';
-
+import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+import SignUpScreen from "./src/views/SignUpScreen";
+import WelcomeScreen from "./src/views/WelcomeScreen";
 
 const MainNavigator = createStackNavigator({
-  Welcome: { screen: WelcomeScreen },
-  SignUp: { screen: SignUpScreen },
+    Welcome: {screen: WelcomeScreen},
+    SignUp: {screen: SignUpScreen},
 });
 
 const App = createAppContainer(MainNavigator);
--- app/Karma/src/views/SignUpScreen.js
+++ app/Karma/src/views/SignUpScreen.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { 
-    View, 
-    Text 
-} from 'react-native';
+import {View, Text} from "react-native";
 
 class SignUpScreen extends React.Component {
-
-    render(){
-        
-        return(
+    render() {
+        return (
             <View>
                 <Text>Hello</Text>
             </View>
-        )
+        );
     }
-
-
 }
-
 
 export default SignUpScreen;
