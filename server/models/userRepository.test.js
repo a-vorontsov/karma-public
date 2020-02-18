681a778c6112d59288d7feb3a9da993368b4c176
Paul <paul.muller@kcl.ac.uk>
2020-02-18 22:45:40 +0000

    add user table tests

---
+++ server/models/userRepository.test.js
@@ -0,0 +1,49 @@
+const db = require("../database/connection");
+const userRepository = require("./userRepository");
+
+beforeEach(() => {
+    db.query("DELETE FROM \"user\"");
+});
+
+afterEach(() => {
+    db.query("DELETE FROM \"user\"");
+});
+
+test('insert user and findById user work', async () => {
+    const user = {
+        email: "test@gmail.com",
+        username: "test1",
+        password_hash: "password",
+        verified: true,
+        salt: "password",
+        date_registered: "2016-06-22 19:10:25-07",
+    };
+    const insertUserResult = await userRepository.insert(user);
+    const findUserResult = await userRepository.findById(insertUserResult.rows[0].id);
+    expect(insertUserResult.rows[0]).toMatchObject(findUserResult.rows[0]);
+});
+
+test('find all users', async () => {
+    const user1 = {
+        email: "test@gmail.com",
+        username: "test1",
+        password_hash: "password",
+        verified: true,
+        salt: "password",
+        date_registered: "2016-06-22 19:10:25-07",
+    };
+
+    const user2 = {
+        email: "user@gmail.com",
+        username: "user",
+        password_hash: "password",
+        verified: true,
+        salt: "password",
+        date_registered: "2016-06-22 19:10:25-07",
+    };
+    const insertUserResult1 = await userRepository.insert(user1);
+    const insertUserResult2 = await userRepository.insert(user2);
+    const findUserResult = await userRepository.findAll();
+    expect(insertUserResult1.rows[0]).toMatchObject(findUserResult.rows[0]);
+    expect(insertUserResult2.rows[0]).toMatchObject(findUserResult.rows[1]);
+});
