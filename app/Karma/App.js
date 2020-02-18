0e88cbc00add559ed7fba7c9f9681c4a049b7b52
usmanonazim <usmanonazim@gmail.com>
2020-02-18 11:43:05 +0000

    Create initial org sign up page

--- app/Karma/App.js
+++ app/Karma/App.js
@@ -4,8 +4,8 @@ import WelcomeScreen from "./src/views/WelcomeScreen";
 import InitSignUpScreen from "./src/routes/InitSignupScreen";
 import UserSignUpScreen from "./src/views/UserSignUpScreen";
 import OrgSignUpScreen from "./src/views/OrgSignUpScreen";
-import PrivacyScreen from './src/views/PrivacyScreen';
-import TermsScreen from './src/views/TermsScreen';
+import PrivacyScreen from "./src/views/PrivacyScreen";
+import TermsScreen from "./src/views/TermsScreen";
 
 const MainNavigator = createStackNavigator(
     {
