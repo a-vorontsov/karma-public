70991256b79672ce60bcc45a01bcaa3110c9951f
houssam966 <houssammahlous123@gmail.com>
2020-02-18 15:25:23 +0000

    remove unnecessary functions

--- server/test/testHelpers.js
+++ server/test/testHelpers.js
@@ -1,5 +1,4 @@
 const db = require("../database/connection");
-const causeRepository = require("../models/causeRepository");
 
 const address = {
     address_1: "221B Baker St",
@@ -25,14 +24,6 @@ const event = {
     date: "2004-10-19",
     time: "10:23:54",
 };
-const initializeCauseTable = async ()=>{
-    causeRepository.insert('cause1', 'cause1 description');
-    causeRepository.insert('cause2', 'cause2 description');
-    causeRepository.insert('cause3', 'cause3 description');
-};
-const clearCauseTable = async () => {
-    await db.query("DELETE FROM cause");
-};
 
 const clearDatabase = async () => {
     await db.query("DELETE FROM event");
@@ -43,6 +34,4 @@ module.exports = {
     address: address,
     event: event,
     clearDatabase: clearDatabase,
-    clearCauseTable: clearCauseTable,
-    initializeCauseTable: initializeCauseTable,
 };
