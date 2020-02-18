865f180931fe6f2f062d768f6ad0a0166d897d41
houssam966 <houssammahlous123@gmail.com>
2020-02-18 23:49:42 +0000

    add left join db function

--- server/models/eventRepository.js
+++ server/models/eventRepository.js
@@ -6,7 +6,8 @@ const insert = (event) => {
         "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) " +
         "RETURNING *"; // returns passed event with it's id set to corresponding id in database
     const params = [event.name, event.organisation_id, event.address_id, event.women_only, event.spots, event.address_visible,
-        event.minimum_age, event.photo_id, event.physical, event.add_info, event.content, event.date, event.time];
+        event.minimum_age, event.photo_id, event.physical, event.add_info, event.content, event.date, event.time,
+    ];
     return db.query(query, params);
 };
 
@@ -20,12 +21,23 @@ const update = (event) => {
         "photo_id = $6, physical = $7, add_info = $8, content = $9, date = $10, time = $11 WHERE id = $12" +
         "RETURNING *"; // returns passed event with it's id set to corresponding id in database
     const params = [event.name, event.women_only, event.spots, event.address_visible, event.minimum_age, event.photo_id,
-        event.physical, event.add_info, event.content, event.date, event.time, event.id];
+        event.physical, event.add_info, event.content, event.date, event.time, event.id,
+    ];
     return db.query(query, params);
 };
 
+const getAll = () => {
+    const query = "SELECT * FROM event";
+    return db.query(query);
+};
+const getEventsWithLocation = () => {
+    const query = "select id(event),lat,long from address left join event on id(address) = address_id";
+    return db.query(query);
+};
 module.exports = {
     insert: insert,
     findById: findById,
     update: update,
+    getAll: getAll,
+    getEventsWithLocation: getEventsWithLocation,
 };
--- server/routes/event.js
+++ server/routes/event.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const distanceCalculator = require('../distanceCalculator');
 const addressRepository = require("../models/addressRepository");
 const eventRepository = require("../models/eventRepository");
 const causeRepository = require("../models/causeRepository");
@@ -7,10 +8,13 @@ const causeRepository = require("../models/causeRepository");
 router.post('/', (req, res) => {
     const address = req.body.address;
     let eventPromise;
-    if (!req.body.address_id) {// address doesn't exist in database yet
+    if (!req.body.address_id) { // address doesn't exist in database yet
         eventPromise = addressRepository.insert(address)
             .then(addressResult => {
-                const event = {...req.body, address_id: addressResult.rows[0].id};
+                const event = {
+                    ...req.body,
+                    address_id: addressResult.rows[0].id,
+                };
                 return eventRepository.insert(event);
             });
     } else {
@@ -29,6 +33,16 @@ router.post('/update', (req, res) => {
         .catch(err => res.status(500).send(err));
 });
 
+router.get('/', (req, res) => {
+    eventRepository.getEventsWithLocation()
+        .then(result => {
+            if (result.rows.length == 0) return res.status(404).send("No events");
+            res.status(200).json(result.rows);
+        })
+        // eventRepository.getAll()
+        //     .then(result => res.status(200).json(result.rows))
+        .catch(err => res.status(500).send(err));
+});
 /**
  * get called when causes tab is pressed in main page
  * returns all causes selected by user
