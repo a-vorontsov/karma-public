708e2e82ca3ad3fd5de414600059d40da6d8a330
Daniel Almasi <almd@pm.me>
2020-02-19 14:46:16 +0000

    Update style to match eslint rules #3
    Update style to match eslint rules #3

--- server/authentication/passport-google.js
+++ server/authentication/passport-google.js
@@ -7,25 +7,25 @@ const users = require("../authentication/user-agent");
  * @param {} passport
  */
 function initialise(passport) {
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://127.0.0.1:8000/auth/google/callback"
-      },
-      function(token, tokenSecret, profile, done) {
-        const user = users.findByEmail(profile.email);
-        if (user !== null) {
-          return done(null, user);
-        } else {
-          return done(null, false, {
-            message: "Email / user does not exist"
-          });
-        }
-      }
-    )
-  );
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                callbackURL: "http://127.0.0.1:8000/auth/google/callback",
+            },
+            function(token, tokenSecret, profile, done) {
+                const user = users.findByEmail(profile.email);
+                if (user !== null) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, {
+                        message: "Email / user does not exist",
+                    });
+                }
+            },
+        ),
+    );
 }
 
 module.exports = initialise;
