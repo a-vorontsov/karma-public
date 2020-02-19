b481fca5def64fb07de0d475470ceb73d15411b6
Daniel Almasi <almd@pm.me>
2020-02-19 00:16:57 +0000

    Initialise JWT as a separate module
    Initialise a module for JTW mechanisms

--- server/authentication/jwt-agent.js
+++ server/authentication/jwt-agent.js
@@ -5,28 +5,50 @@ const tokenExpiry = "1h";
 const algo = "ES512";
 const privateKey = fs.readFileSync("./priv.key", "utf8");
 const publicKey = fs.readFileSync("./pub.key", "utf8");
+const defaultTokenSubject = "Karma app communication";
+const defaultTokenAudiance = "user";
 
-var toeknSubject = "some subj";
-var tokenAudiance = "user";
+// var payload = {
+//   data1: "Data 1",
+//   data2: "Data 2",
+//   data3: "Data 3",
+//   data4: true
+// };
 
-var payload = {
-  data1: "Data 1",
-  data2: "Data 2",
-  data3: "Data 3",
-  data4: true
-};
-
-var signOptions = {
- issuer: toeknIssuer,
- toeknSubject: toeknSubject,
- audience: tokenAudiance,
- expiresIn:  tokenExpiry,
- algorithm:  algo
-};
+// var signOptions = {
+//   issuer: toeknIssuer,
+//   subject: tokenSubject,
+//   audience: tokenAudiance,
+//   expiresIn: tokenExpiry,
+//   algorithm: algo
+// };
 
 /**
  * ECDSA using P-521 curve and SHA-512 hash algorithm
  */
-var token = jwt.sign(payload,
-  privateKey, signOptions
-);
+// var token = jwt.sign(payload, privateKey, signOptions);
+
+function sign(tokenSubject, tokenAudiance, payload) {
+    return jwt.sign(payload, privateKey, {
+        issuer: toeknIssuer,
+        subject: tokenSubject,
+        audience: tokenAudiance,
+        expiresIn: tokenExpiry,
+        algorithm: algo
+    })
+}
+
+function signWithDefaultParams(payload) {
+  return jwt.sign(payload, privateKey, {
+    issuer: toeknIssuer,
+    subject: defaultTokenSubject,
+    audience: defaultTokenAudiance,
+    expiresIn: tokenExpiry,
+    algorithm: algo
+  });
+}
+
+module.exports = {
+    sign: sign,
+    signWithDefaultParams: signWithDefaultParams
+}
