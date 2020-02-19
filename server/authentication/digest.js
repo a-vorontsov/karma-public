df5297d1f8cb4f7338449ac84a0567aa32bccc25
Daniel Almasi <almd@pm.me>
2020-02-19 14:53:20 +0000

    Update style to match eslint rules #4
    Update style to match eslint rules #4

--- server/authentication/digest.js
+++ server/authentication/digest.js
@@ -3,52 +3,52 @@ const crypto = require("crypto");
 /**
  * Generate a secure random salt of schema defined length
  * (256-bits) and return value in hex.
- * @returns 32-byte salt in hex
+ * @return {hex} 32-byte salt in hex
  */
 function getSecureSaltInHex() {
-  return crypto.randomBytes(32).toString("hex");
+    return crypto.randomBytes(32).toString("hex");
 }
 
 /**
  * Generate a secure random salt of schema defined length
  * (256-bits) and return value in Base64.
- * @returns 32-byte salt in base64
+ * @return {base64} 32-byte salt in base64
  */
 function getSecureSaltInBase64() {
-  return crypto.randomBytes(32).toString("base64");
+    return crypto.randomBytes(32).toString("base64");
 }
 
 /**
  * Hashes the given password and salt concatenated with the
  * SHA-256 crypto standard hash function and returns the value in hex.
- * @param password
- * @param secureSalt
- * @returns 32-byte hash in hex
+ * @param {string} password
+ * @param {hex} secureSalt
+ * @return {hex} 32-byte hash in hex
  */
 function hashPassWithSaltInHex(password, secureSalt) {
-  return crypto
-    .createHash("sha256")
-    .update(password + secureSalt)
-    .digest("hex");
+    return crypto
+        .createHash("sha256")
+        .update(password + secureSalt)
+        .digest("hex");
 }
 
 /**
  * Hashes the given password and salt concatenated with the
  * SHA-256 crypto standard hash function and returns the value in Base64.
- * @param password
- * @param secureSalt
- * @returns 32-byte hash in Base64
+ * @param {string} password
+ * @param {base64} secureSalt
+ * @return {base64} 32-byte hash in Base64
  */
 function hashPassWithSaltInBase64(password, secureSalt) {
-  return crypto
-    .createHash("sha256")
-    .update(password + secureSalt)
-    .digest("base64");
+    return crypto
+        .createHash("sha256")
+        .update(password + secureSalt)
+        .digest("base64");
 }
 
 module.exports = {
-  getSecureSaltInHex: getSecureSaltInHex,
-  getSecureSaltInBase64: getSecureSaltInBase64,
-  hashPassWithSaltInHex: hashPassWithSaltInHex,
-  hashPassWithSaltInBase64: hashPassWithSaltInBase64
+    getSecureSaltInHex: getSecureSaltInHex,
+    getSecureSaltInBase64: getSecureSaltInBase64,
+    hashPassWithSaltInHex: hashPassWithSaltInHex,
+    hashPassWithSaltInBase64: hashPassWithSaltInBase64,
 };
--- server/authentication/passport-config.js
+++ server/authentication/passport-config.js
@@ -2,12 +2,12 @@ const passport = require("passport");
 const userAgent = require("./user-agent");
 const initialiseLocal = require("./passport-local");
 initialiseLocal(
-  passport,
-  email => userAgent.findByEmail(email),
-  id => userAgent.findById(id)
+    passport,
+    email => userAgent.findByEmail(email),
+    id => userAgent.findById(id),
 );
 if (process.env.ENABLE_OAUTH === "1") {
-  require("./passport-google")(passport);
-  require("./passport-facebook")(passport);
-  require("./passport-linkedin")(passport);
+    require("./passport-google")(passport);
+    require("./passport-facebook")(passport);
+    require("./passport-linkedin")(passport);
 }
