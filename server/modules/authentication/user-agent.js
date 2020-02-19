1be42478cbab8a37106972570c0c243579ab9294
Daniel Almasi <almd@pm.me>
2020-02-19 23:37:29 +0000

    Add individual registration boiler plate
    Add individual registration boiler plate

--- server/modules/authentication/user-agent.js
+++ server/modules/authentication/user-agent.js
@@ -7,39 +7,59 @@ let nextId = users.length;
 users.push({
     id: "admin0",
     username: "admin",
-    name: "admin",
     email: "dan@dan.com",
     salt: "d7895ef4ceb93b9808818dd1246026cef2a50a2351b447b29e08319d41b86713",
     password: "bf6d74ab2d96afe33cd66ad1e9dc8eeb2c5e4a1d9d8001a2cb501708a02f1dc4",
 });
 
 /**
- * Register a new user based on the HTTP request.
- * @param {HTTP} req
+ * Returns true if email exists in emails table
+ * @param {string} email
+ * @return {boolean} true if email exists in DB
  */
-function register(req) {
+function emailExists(email) {
+    return findByEmail(email) !== undefined;
+}
+
+/**
+ * Returns true if email verified flag is true
+ * @param {string} email
+ * @return {boolean} true if email is verified
+ */
+function isEmailVerified(email) {
+    return true;
+}
+
+/**
+ * Register a new user with given email, username
+ * and password.
+ * @param {string} email
+ * @param {string} username
+ * @param {string} password
+ */
+function registerUser(email, username, password) {
     const secureSalt = digest.getSecureSaltInHex();
     const hashedPassword = digest.hashPassWithSaltInHex(
-        req.body.password,
+        password,
         secureSalt,
     );
-    pushNewUser(req, secureSalt, hashedPassword);
+    pushNewUser(email, username, secureSalt, hashedPassword);
 }
 
 /**
  * Push a new user to the database.
  * This function should only be accessible from
  * withing this module.
- * @param {HTTP} req
+ * @param {string} email
+ * @param {string} username
  * @param {string} secureSalt 256-bit
  * @param {string} hashedPassword 256-bit
  */
-function pushNewUser(req, secureSalt, hashedPassword) {
+function pushNewUser(email, username, secureSalt, hashedPassword) {
     users.push({
         id: nextId,
-        username: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
+        email: email,
+        username: username,
         salt: secureSalt,
         password: hashedPassword,
     });
@@ -48,13 +68,34 @@ function pushNewUser(req, secureSalt, hashedPassword) {
 }
 
 /**
+ * Register a new individual
+ * @param {integer} id
+ * @param {string} foreName
+ * @param {string} surName
+ * @param {date} birthDate
+ */
+function registerIndividual(id, foreName, surName, birthDate) {
+    // TODO:
+}
+
+/**
+ * Register a new organisation
+ * @param {integer} id
+ * @param {string} name
+ * @param {string} orgNumber
+ */
+function registerOrg(id, name, orgNumber) {
+    // TODO:
+}
+
+/**
  * Change a user's password
- * @param {Object} user
+ * @param {integer} id
  * @param {string} secureSalt 256-bit
  * @param {string} hashedPassword 256-bit
  */
-function changePassword(user, secureSalt, hashedPassword) {
-    // TODO: push
+function changePassword(id, secureSalt, hashedPassword) {
+    const user = findById(id);
     user.salt = secureSalt;
     user.password = hashedPassword;
 }
@@ -113,7 +154,7 @@ function userExists(id) {
 }
 
 module.exports = {
-    register: register,
+    registerUser: registerUser,
     findByEmail: findByEmail,
     findByUsername: findByUsername,
     findById: findById,
