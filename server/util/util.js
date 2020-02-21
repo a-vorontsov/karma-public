98fef29c4cc5c2cf0e7308d072d21d29a70ea56d
Sten Laane <sten.laane@kcl.ac.uk>
2020-02-21 00:05:24 +0000

    Add util module for common utility functions (e. g. checking if a user is an individual or organisation)

---
+++ server/util/util.js
@@ -0,0 +1,26 @@
+const userRepository = require("../models/userRepository");
+const individualRepository = require("../models/individualRepository");
+const organisationRepository = require("../models/organisationRepository");
+
+const isIndividual = async (userId) => {
+    const userResult = await userRepository.findById(userId);
+    if (userResult.rows.length === 0) {
+        throw Error(`No user with id ${userId} exists`);
+    }
+    const individualResult = await individualRepository.findByUserID(userId);
+    return individualResult.rows.length > 0; // found at least one individual with userId
+};
+
+const isOrganization = async (userId) => {
+    const userResult = await userRepository.findById(userId);
+    if (userResult.rows.length === 0) {
+        throw Error(`No user with id ${userId} exists`);
+    }
+    const organisationResult = await organisationRepository.findByUserID(userId);
+    return organisationResult.rows.length > 0; // found at least one organisation with userId
+};
+
+module.exports = {
+    isIndividual: isIndividual,
+    isOrganization: isOrganization,
+};
