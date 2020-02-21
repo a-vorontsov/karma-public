1cf12f58dbd5c8d94b0bd70e7e95e7821c02f0fe
Paul <paul.muller@kcl.ac.uk>
2020-02-21 17:39:43 +0000

    add additional tests for error handling

--- server/models/databaseRepositories/addressRepository.js
+++ server/models/databaseRepositories/addressRepository.js
@@ -16,13 +16,17 @@ const findById = async (id) => {
     return queryResult;
 };
 
-const update = (address) => {
+const update = async (address) => {
     const query = "UPDATE address SET address_1 = $1, address_2 = $2, postcode = $3, city = $4, region = $5, " +
         "lat = $6, long = $7 WHERE id = $8" +
-        "RETURNING *"; // returns passed event with it's id set to corresponding id in database
+        "RETURNING *"; // returns passed address with it's id set to corresponding id in database
     const params = [address.address_1, address.address_2, address.postcode, address.city, address.region, address.lat,
         address.long, address.id];
-    return db.query(query, params);
+    const queryResult = await db.query(query, params);
+    if(queryResult.rowCount === 0) {
+        throw Error(`No address with id ${address.id} exists`);
+    }
+    return queryResult;
 };
 
 module.exports = {
--- server/models/databaseRepositories/addressRepository.test.js
+++ server/models/databaseRepositories/addressRepository.test.js
@@ -15,6 +15,13 @@ test('findByID throws error', async () => {
     await expect(addressRepository.findById(0)).rejects.toThrow('No address with id 0 exists');
 });
 
+test('update throws error', async () => {
+    const insertAddressResult = await addressRepository.insert(address);
+    const insertedAddress = insertAddressResult.rows[0];
+    insertedAddress.id = -1;
+    await expect(addressRepository.update(insertedAddress)).rejects.toThrow(`No address with id -1 exists`);
+});
+
 test('insert and findById work', async  () => {
     const insertResult = await addressRepository.insert(address);
     const findResult = await addressRepository.findById(insertResult.rows[0].id);
