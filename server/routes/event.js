53fec59475985a7af7f6273f722ca2bcaaadd589
houssam966 <houssammahlous123@gmail.com>
2020-02-22 14:14:04 +0000

    group events by cause

--- server/routes/event.js
+++ server/routes/event.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const distanceCalculator = require('../distanceCalculator');
 const addressRepository = require("../models/addressRepository");
 const eventRepository = require("../models/eventRepository");
 const userRepository = require("../models/userRepository");
 const selectedCauseRepository = require("../models/selectedCauseRepository");
+const eventSorter = require("../sorting/event");
+
 
 router.post('/', (req, res) => {
     const address = req.body.address;
@@ -45,28 +46,15 @@ router.get('/', async (req, res) => {
     if (!user) return res.status(400).send("No user with specified id");
     eventRepository.getEventsWithLocation()
         .then(result => {
-            if (result.rows.length == 0) return res.status(404).send("No events");
             const events = result.rows;
-            // sort by time
-            events.sort((event1, event2) => {
-                const date1 = new Date(event1.date);
-                const date2 = new Date(event2.date);
-                if (date1 > date2) return 1;
-                else if (date1 < date2) return -1;
-                else return 0;
-            });
-            // sort by date
-            events.sort((event1, event2) => {
-                event1.distance = distanceCalculator.getDistance(user, event1, 'M');
-                event2.distance = distanceCalculator.getDistance(user, event2, 'M');
-                if (event1.distance > event2.distance) return 1;
-                else if (event1.distance < event2.distance) return -1;
-                else return 0;
-            });
+            if (events.length === 0) return res.status(404).send("No events");
+            eventSorter.sortByTime(events);
+            eventSorter.sortByDistanceFromUser(events, user);
             res.status(200).json(events);
         })
         .catch(err => res.status(500).send(err));
 });
+
 /**
  * route {GET} event/causes
  * @param {Object} req - Express request object
@@ -77,14 +65,20 @@ router.get('/', async (req, res) => {
  *  status: 400, description: if ID param is not specified or in wrong format/NaN
  *  status: 500, description: Most probably a database error occured
  */
-router.get('/causes', (req, res) => {
+router.get('/causes', async (req, res) => {
     const userId = req.query.userId;
     if (!userId) return res.status(400).send("No user id was specified in the query");
     if (isNaN(userId)) return res.status(400).send("ID specified is in wrong format");
+    const userResult = await userRepository.getUserLocation(userId);
+    const user = userResult.rows[0];
+    if (!user) return res.status(400).send("No user with specified id");
     selectedCauseRepository.findEventsSelectedByUser(userId)
         .then(result => {
-            if (result.rows.length === 0) return res.status(404).send("No causes selected by user");
-            res.status(200).json(result.rows);
+            const events = result.rows;
+            if (events.length === 0) return res.status(404).send("No causes selected by user");
+            eventSorter.sortByTime(events);
+            eventSorter.sortByDistanceFromUser(events, user);
+            res.status(200).json(eventSorter.groupByCause(events));
         })
         .catch(err => res.status(500).send(err));
 });
