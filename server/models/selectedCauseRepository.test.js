f8d1c2739e465ad0b8cfba7f79b19ea97891d86b
houssam966 <houssammahlous123@gmail.com>
2020-02-22 10:04:37 +0000

    uncomment insert test

--- server/models/selectedCauseRepository.test.js
+++ server/models/selectedCauseRepository.test.js
@@ -1,36 +1,36 @@
-// const testHelpers = require("../test/testHelpers");
-// const selectedCauseRepository = require("./selectedCauseRepository");
-// const causeRepository = require("./causeRepository");
-// const userRepository = require("./userRepository");
-// const registrationRepository = require("./registrationRepository");
+const testHelpers = require("../test/testHelpers");
+const selectedCauseRepository = require("./selectedCauseRepository");
+const causeRepository = require("./causeRepository");
+const userRepository = require("./userRepository");
+const registrationRepository = require("./registrationRepository");
 
 
-// const registration = testHelpers.registration3;
-// const cause = testHelpers.cause;
-// const user = testHelpers.user3;
+const registration = testHelpers.registration3;
+const cause = testHelpers.cause;
+const user = testHelpers.user3;
 
-// beforeEach(() => {
-//     return testHelpers.clearDatabase();
-// });
+beforeEach(() => {
+    return testHelpers.clearDatabase();
+});
 
-// afterEach(() => {
-//     user.email = "";
-//     return testHelpers.clearDatabase();
-// });
+afterEach(() => {
+    user.email = "";
+    return testHelpers.clearDatabase();
+});
 
-// test('insert works', async () => {
-//     const insertRegistrationResult = await registrationRepository.insert(registration);
-//     user.email = insertRegistrationResult.rows[0].email;
-//     const insertUserResult = await userRepository.insert(user);
-//     const insertCauseResult = await causeRepository.insert(cause)
-//     const user_id = insertUserResult.rows[0].id;
-//     const cause_id = insertCauseResult.rows[0].id;
-//     const insertResult = await selectedCauseRepository.insert(user_id, cause_id);
-//     expect(insertResult.rows[0]).toMatchObject({
-//         'user_id': user_id,
-//         'cause_id': cause_id
-//     });
-// });
+test('insert works', async () => {
+    const insertRegistrationResult = await registrationRepository.insert(registration);
+    user.email = insertRegistrationResult.rows[0].email;
+    const insertUserResult = await userRepository.insert(user);
+    const insertCauseResult = await causeRepository.insert(cause)
+    const user_id = insertUserResult.rows[0].id;
+    const cause_id = insertCauseResult.rows[0].id;
+    const insertResult = await selectedCauseRepository.insert(user_id, cause_id);
+    expect(insertResult.rows[0]).toMatchObject({
+        'user_id': user_id,
+        'cause_id': cause_id
+    });
+});
 // test('find works', async () => {
 //     const insertRegistrationResult = await registrationRepository.insert(registration);
 //     user.email = insertRegistrationResult.rows[0].email;
