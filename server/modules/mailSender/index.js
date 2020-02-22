7368623daffa7ba995b6ba4b067a8d4f2218c1c4
Daniel Almasi <almd@pm.me>
2020-02-22 17:59:23 +0000

    Update bug report sender
    Update bug report sender

--- server/modules/mailSender/index.js
+++ server/modules/mailSender/index.js
@@ -8,11 +8,11 @@ const transporter = nodemailer.createTransport({
     },
 });
 module.exports = {
-    sendToken: (email, token)=>{
+    sendToken: (email, token) => {
         const mailOptions = {
             from: `${process.env.EMAIL_ADDRESS}`,
             to: `${email}`,
-            subject: 'Reset Password Verification Code',
+            subject: "Reset Password Verification Code",
             text: `K-${token} is your karma verification code`,
         };
         console.log(`Sending mail to ${email}`);
@@ -21,6 +21,26 @@ module.exports = {
                 throw new Error(err.message);
             } else {
                 console.log(`Email with token sent to ${email}`);
+            }
+        });
+    },
+    sendBugReport: (email, report) => {
+        const mailOptions = {
+            from: `${email}`,
+            to: `${process.env.EMAIL_ADDRESS}`,
+            subject: "Bug Report",
+            text: `Bug report from ${email}: ${report}`,
+        };
+        console.log(
+            `Sending bug report from ${email} to ${process.env.EMAIL_ADDRESS}`,
+        );
+        transporter.sendMail(mailOptions, (err, response) => {
+            if (err) {
+                throw new Error(err.message);
+            } else {
+                console.log(
+                    `Bug report from ${email} to ${process.env.EMAIL_ADDRESS} sent successfully.`,
+                );
             }
         });
     },
--- server/routes/bugreport.js
+++ server/routes/bugreport.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const mailSender = require("../modules/mailSender/index");
 
 /**
- * Attempt to store bug report in database.
- * Returns result of operation.
- * @param {HTTP} req must contain emailAddr, reportBody
- * @return {HTTP} res
+ * Attempt send a bug report to admin email account.
+ * This requires a contact email address to be provided/input
+ * by the app user (as bugs may occur when a user is not
+ * signed-in). Returns success or an error message from
+ * the mailSender module.
+ * @param {HTTP} req
+ * @param {string} email of user (typed-in)
+ * @param {string} report bug description (typed-in)
+ * @return {HTTP} one of the following HTTP responses:
+ * - if email successfully sent, 200 - bug report sent
+ * - if any error, 400 - error msg from mailSender module
  */
 router.post("/", async (req, res) => {
     try {
-        pushPugReport(req);
-        res.status(200).send({message: "Successful registration."});
+        mailSender.sendBugReport(req.body.email, req.body.report);
+        res.status(200).send({message:
+            "Bug report sent.",
+        });
     } catch (e) {
-        res.status(400).send({message: e.message});
+        res.status(400).send({
+            message: e.message,
+        });
     }
 });
-
-/**
- * Push bug report to database
- * @param {JSON} req
- */
-function pushPugReport(req) {
-    // TODO:
-    // req.reportBody
-}
 
 module.exports = router;
