c0c2596b5d67655e58faf16824ea46c8e2b0e449
Houssam <44209131+houssam966@users.noreply.github.com>
2020-02-23 23:32:40 +0000

    Update index.js

--- server/pagination/index.js
+++ server/pagination/index.js
@@ -1,20 +1,9 @@
-const {
-    calculateLimitAndOffset,
-    paginate,
-} = require('paginate-info');
+const {calculateLimitAndOffset, paginate} = require('paginate-info');
 
 module.exports = {
     getPageData: (req, data) => {
-        const {
-            query: {
-                currentPage,
-                pageSize,
-            },
-        } = req;
-        const {
-            limit,
-            offset,
-        } = calculateLimitAndOffset(currentPage, pageSize);
+        const {query: {currentPage, pageSize}} = req;
+        const {limit, offset} = calculateLimitAndOffset(currentPage, pageSize);
         const count = data.length;
         const paginatedData = data.slice(offset, offset + limit);
         const paginationInfo = paginate(currentPage, count, paginatedData);
@@ -25,3 +14,4 @@ module.exports = {
         return result;
     },
 };
+
