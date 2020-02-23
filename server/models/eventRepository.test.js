9934d4cdefd76e5214945b0ec731eea2fe70a1df
houssam966 <houssammahlous123@gmail.com>
2020-02-23 23:42:49 +0000

    update tests

--- server/models/eventRepository.test.js
+++ server/models/eventRepository.test.js
@@ -1,43 +1,49 @@
-const db = require("../database/connection");
+const addressRepository = require("./addressRepository");
 const eventRepository = require("./eventRepository");
+const testHelpers = require("../test/testHelpers");
+const userRepository = require("./userRepository");
+const registrationRepository = require("./registrationRepository");
+
+const registration = testHelpers.registration;
+const address = testHelpers.address;
+const event = testHelpers.event;
+const user = testHelpers.user;
 
 beforeEach(() => {
-    db.query("DELETE FROM event");
-    db.query("DELETE FROM address");
+    return testHelpers.clearDatabase();
 });
 
 afterEach(() => {
-    db.query("DELETE FROM event");
-    db.query("DELETE FROM address");
+    user.email = "";
+    event.address_id = -1;
+    event.user_id = -1;
+    return testHelpers.clearDatabase();
 });
 
 test('insert and findById work', async () => {
-    const address = {
-        address_1: "Line 1",
-        address_2: "Line 2",
-        postcode: "14 aa",
-        city: "LDN",
-        region: "LDN again",
-        lat: "0.3",
-        long: "0.5"
-    };
-    const event = {
-        address: address,
-        name: "event",
-        organization_id: 3,
-        address_id: 3,
-        women_only: true,
-        spots: 3,
-        address_visible: true,
-        minimum_age: 16,
-        photo_id: true,
-        physical: true,
-        add_info: true,
-        content: "fun event yay",
-        date: "2004-10-19",
-        time: "10:23:54"
-    };
+    const insertRegistrationResult = await registrationRepository.insert(registration);
+    user.email = insertRegistrationResult.rows[0].email;
+    const insertAddressResult = await addressRepository.insert(address);
+    const insertUserResult = await userRepository.insert(user);
+    event.address_id = insertAddressResult.rows[0].id;
+    event.user_id = insertUserResult.rows[0].id;
     const insertEventResult = await eventRepository.insert(event);
     const findEventResult = await eventRepository.findById(insertEventResult.rows[0].id);
-    expect(insertEventResult.rows[0]).toMatchObject(findEventResult.rows[0]);
+    expect(findEventResult.rows[0]).toMatchObject(insertEventResult.rows[0]);
+});
+
+test('update works', async () => {
+    const insertRegistrationResult = await registrationRepository.insert(registration);
+    user.email = insertRegistrationResult.rows[0].email;
+    const insertAddressResult = await addressRepository.insert(address);
+    const insertUserResult = await userRepository.insert(user);
+    event.address_id = insertAddressResult.rows[0].id;
+    event.user_id = insertUserResult.rows[0].id;
+
+    const insertEventResult = await eventRepository.insert(event);
+    const insertedEvent = insertEventResult.rows[0];
+    insertedEvent.name = "New name";
+    insertedEvent.spots = 5;
+    const updateEventResult = await eventRepository.update(insertedEvent);
+    expect(updateEventResult.rows[0]).toMatchObject(insertedEvent);
 });
