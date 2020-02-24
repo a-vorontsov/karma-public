efff99424c767bd57574d37286a1ff84995d4814
Sten Laane <sten.laane@kcl.ac.uk>
2020-02-24 15:41:22 +0000

    Add SQL queries for signing up to an event

--- server/models/individualRepository.test.js
+++ server/models/individualRepository.test.js
@@ -21,7 +21,6 @@ afterEach(() => {
 });
 
 test('insert individual and findById individual work', async () => {
-
     const insertRegistrationRepository = await registrationRepository.insert(registration);
     user.email = insertRegistrationRepository.rows[0].email;
     const insertUserResult = await userRepository.insert(user);
