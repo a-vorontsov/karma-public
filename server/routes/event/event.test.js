9a08aadf85812b4a9a97c90111e2af832b8696bd
Daniel Almasi <almd@pm.me>
2020-02-24 16:24:58 +0000

    Fix event route
    Fix event route

--- server/routes/event/event.test.js
+++ server/routes/event/event.test.js
@@ -1,20 +1,22 @@
-const request = require('supertest');
-const app = require('../../app');
-const testHelpers = require("../../test/testHelpers");
+const request = require("supertest");
+const app = require("../app");
+const testHelpers = require("../test/testHelpers");
+const util = require("../util/util");
 
-const addressRepository = require("../../models/addressRepository");
-const eventRepository = require("../../models/eventRepository");
+const addressRepository = require("../models/addressRepository");
+const eventRepository = require("../models/eventRepository");
 
 jest.mock("../models/eventRepository");
 jest.mock("../models/addressRepository");
+jest.mock("../util/util");
 
 beforeEach(() => {
-    return testHelpers.clearDatabase();
+  return testHelpers.clearDatabase();
 });
 
 afterEach(() => {
-    jest.clearAllMocks();
-    return testHelpers.clearDatabase();
+  jest.clearAllMocks();
+  return testHelpers.clearDatabase();
 });
 
 const address = testHelpers.address;
@@ -22,41 +24,128 @@ const event = testHelpers.event;
 event.organization_id = 1;
 event.address_id = 1;
 
+test("creating event with known address works", async () => {
+  eventRepository.insert.mockResolvedValue({
+    rows: [
+      {
+        ...event,
+        id: 1
+      }
+    ]
+  });
+  const response = await request(app)
+    .post("/event")
+    .send(event);
 
-test('creating event with known address works', async () => {
-    eventRepository.insert.mockResolvedValue({rows: [{...event, id: 1}]});
-    const response = await request(app).post("/events").send(event);
-
-    expect(eventRepository.insert).toHaveBeenCalledTimes(1);
-    expect(addressRepository.insert).toHaveBeenCalledTimes(0);
-    expect(response.body).toMatchObject({...event, id: 1});
-    expect(response.statusCode).toBe(200);
+  expect(eventRepository.insert).toHaveBeenCalledTimes(1);
+  expect(addressRepository.insert).toHaveBeenCalledTimes(0);
+  expect(response.body).toMatchObject({
+    ...event,
+    id: 1
+  });
+  expect(response.statusCode).toBe(200);
 });
 
-test('updating events works', async () => {
-    const mockAddress = {...address, id: 1};
-    addressRepository.update.mockResolvedValue({rows: [mockAddress]});
-    eventRepository.update.mockResolvedValue({rows: [{...event, id: 1}]});
+test("updating events works", async () => {
+  const mockAddress = {
+    ...address,
+    id: 1
+  };
+  addressRepository.update.mockResolvedValue({
+    rows: [mockAddress]
+  });
+  eventRepository.update.mockResolvedValue({
+    rows: [
+      {
+        ...event,
+        id: 3
+      }
+    ]
+  });
 
-    const response = await request(app).post("/events/update").send(event);
+  const response = await request(app)
+    .post("/event/update/3")
+    .send({
+      event,
+      address: mockAddress
+    });
 
-    expect(eventRepository.update).toHaveBeenCalledTimes(1);
-    expect(addressRepository.update).toHaveBeenCalledTimes(1);
-    expect(response.body).toMatchObject({...event, id: 1});
-    expect(response.statusCode).toBe(200);
+  expect(eventRepository.update).toHaveBeenCalledTimes(1);
+  expect(addressRepository.update).toHaveBeenCalledTimes(1);
+  expect(response.body).toMatchObject({
+    ...event,
+    id: 3
+  });
+
+  expect(response.statusCode).toBe(200);
 });
 
-test('creating event with no address_id creates new address and event', async () => {
-    const eventNoAddressId = {...event};
-    delete eventNoAddressId.address_id;
-    const mockAddress = {...address, id: 1};
-    addressRepository.insert.mockResolvedValue({rows: [mockAddress]});
-    eventRepository.insert.mockResolvedValue({rows: [{...event, id: 1}]});
+test("requesting specific event data works", async () => {
+  eventRepository.findById.mockResolvedValue({
+    rows: [
+      {
+        ...event,
+        id: 3
+      }
+    ]
+  });
+  addressRepository.findById.mockResolvedValue({
+    rows: [address]
+  });
+  const response = await request(app).get("/event/3");
 
-    const response = await request(app).post("/events").send(eventNoAddressId);
+  expect(eventRepository.findById).toHaveBeenCalledTimes(1);
+  expect(eventRepository.findById).toHaveBeenCalledWith("3");
+  expect(addressRepository.findById).toHaveBeenCalledWith(event.address_id);
+  expect(response.statusCode).toBe(200);
+  expect(response.body).toMatchObject({
+    ...event,
+    id: 3
+  });
+});
 
-    expect(eventRepository.insert).toHaveBeenCalledTimes(1);
-    expect(addressRepository.insert).toHaveBeenCalledTimes(1);
-    expect(response.body).toMatchObject({...event, id: 1});
-    expect(response.statusCode).toBe(200);
+test("error returned when user tries to exceed monthly event creation limit", async () => {
+  util.isIndividual.mockResolvedValue(true);
+  eventRepository.findAllByUserId.mockResolvedValue({
+    rows: [{}, {}, {}, {}]
+  }); // 4 events
+  const response = await request(app)
+    .post("/event")
+    .send(event);
+  expect(response.statusCode).toBe(400);
+});
+
+test("creating event with no address_id creates new address and event", async () => {
+  util.isIndividual.mockResolvedValue(false);
+  const eventNoAddressId = {
+    ...event
+  };
+  delete eventNoAddressId.address_id;
+  const mockAddress = {
+    ...address,
+    id: 1
+  };
+  addressRepository.insert.mockResolvedValue({
+    rows: [mockAddress]
+  });
+  eventRepository.insert.mockResolvedValue({
+    rows: [
+      {
+        ...event,
+        id: 1
+      }
+    ]
+  });
+
+  const response = await request(app)
+    .post("/event")
+    .send(eventNoAddressId);
+
+  expect(eventRepository.insert).toHaveBeenCalledTimes(1);
+  expect(addressRepository.insert).toHaveBeenCalledTimes(1);
+  expect(response.body).toMatchObject({
+    ...event,
+    id: 1
+  });
+  expect(response.statusCode).toBe(200);
 });
