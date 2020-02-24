ef59984470db1b0c465eb04df7818f6e2579b7cd
houssam966 <houssammahlous123@gmail.com>
2020-02-24 00:11:00 +0000

    fix eslint error

--- server/models/individualRepository.js
+++ server/models/individualRepository.js
@@ -37,7 +37,8 @@ const findFavouriteEvents = (userId) => {
 const findGoingEvents = (userId) => {
     const query = "select id(event) as event_id,name,women_only,spots,address_visible,minimum_age,photo_id," +
         "physical,add_info,content,date,user_id as event_creator_id,address_1,address_2,postcode,city,region,lat,long " +
-        "from sign_up left join event on event_id = id(event) left join address on id(address) = address_id where individual_id = $1 and confirmed = true";
+        "from sign_up left join event on event_id = id(event)" +
+        "left join address on id(address) = address_id where individual_id = $1 and confirmed = true";
     return db.query(query, [userId]);
 };
 
