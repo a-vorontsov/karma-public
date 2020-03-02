6d9e341a65a5e5e780aa7b95dc3eaa59945d3d50
houssam966 <houssammahlous123@gmail.com>
2020-03-02 11:05:31 +0000

    fix where clause in selectedCauseRepository

--- server/models/selectedCauseRepository.js
+++ server/models/selectedCauseRepository.js
@@ -44,7 +44,9 @@ const find = (userID, causeID) => {
 };
 
 const findEventsSelectedByUser = (userID, filters) => {
-    const whereClause = filterer.getWhereClause(filters);
+    let whereClause = filterer.getWhereClause(filters);
+    if (whereClause === "") whereClause = "where ";
+    else whereClause += " and ";
     const query = "select id(event),name(event),address_id,women_only,spots,address_visible,minimum_age,photo_id," +
         "physical add_info,content,date,cause_id(event_cause),name(cause) as cause_name,description as cause_description," +
         "user_id(event) as event_creator_id,address_1,address_2,postcode,city,region,lat,long from event " +
@@ -52,7 +54,8 @@ const findEventsSelectedByUser = (userID, filters) => {
         "right join selected_cause on cause_id(event_cause)=cause_id(selected_cause) " +
         "left join cause on cause_id(event_cause) = id(cause) " +
         "left join address on id(address) = address_id " +
-        whereClause + " and user_id(selected_cause) = $1";
+        whereClause + "user_id(selected_cause) = $1";
+    console.log(query);
     return db.query(query, [userID]);
 };
 
