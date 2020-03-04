4e75ebe7750f373c6bdae4fb6f80427694f6998a
houssam966 <houssammahlous123@gmail.com>
2020-03-04 12:12:27 +0000

    add jsdoc comments

--- server/routes/signin/forgotPassword.js
+++ server/routes/signin/forgotPassword.js
@@ -1,3 +1,7 @@
+/**
+ * @module signin-forgotPassword
+ */
+
 const express = require('express');
 const router = express.Router();
 const randomize = require('randomatic');
@@ -5,11 +9,18 @@ const mailSender = require('../../modules/mailSender');
 const userRepository = require("../../models/databaseRepositories/userRepository");
 const util = require("../../util/util");
 
-router.get('/', (req, res) => {
-    res.send('forgot password screen');
-});
-
-// gets called when the person submits the forgot password button
+/**
+ * Endpoint called whenever a user requests a reset password token.<br/>
+ * URL example: POST http://localhost:8000/signin/forgot
+ * @param {String} req.body.email - Email of the user
+ * @returns
+ *  status: 200, description: Token sent successfully <br/>
+ *  status: 400, description: Email not specified in request body <br/>
+ *  status: 404, description: User with email specified not found <br/>
+ *  status: 500, description: DB error or error in sending email
+ *  @name Forgot password
+ *  @function
+ */
 router.post('/', async (req, res) => {
     const email = req.body.email;
     const checkEmailResult = await util.checkEmail(email);
@@ -26,7 +37,20 @@ router.post('/', async (req, res) => {
         .catch(err => res.status(500).send(err));
 });
 
-// gets called when user writes in the token they recieved and click submit
+/**
+ * Endpoint called whenever a user writes in the token they recieved and click submit.<br/>
+ * URL example: POST http://localhost:8000/signin/forgot
+ * @param {String} req.body.email - Email of the user
+ * @param {String} req.body.token - Token input by user
+ * @returns
+ *  status: 200, description: Token is accepted <br/>
+ *  status: 400, description: Email or Token not specified in request body <br/>
+ *  status: 401, description: Token did not match sent token or toke expired <br/>
+ *  status: 404, description: Token sent to specified email not found <br/>
+ *  status: 500, description: DB error
+ *  @name Confirm token
+ *  @function
+ */
 router.post('/confirm', async (req, res) => {
     const tokenRecieved = req.body.token;
     const email = req.body.email;
@@ -38,7 +62,7 @@ router.post('/confirm', async (req, res) => {
 
     userRepository.findResetToken(checkEmailResult.user.id)
         .then(result => {
-            if (result.rows.length == 0) return res.status(400).send("No token sent to " + email);
+            if (result.rows.length == 0) return res.status(404).send("No token sent to " + email);
 
             const tokenSent = result.rows[0].password_token;
             const expiryDate = result.rows[0].expiry_date;
