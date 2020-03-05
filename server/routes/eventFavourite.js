7bc5896471b5dc13e928eaae6d3e8b1f7d451f4c
Paul <paul.muller@kcl.ac.uk>
2020-03-05 19:06:09 +0000

    Eslint fixes

--- server/routes/eventFavourite.js
+++ server/routes/eventFavourite.js
@@ -4,7 +4,6 @@
 
 const express = require('express');
 const router = express.Router();
-const eventRepository = require("../models/databaseRepositories/eventRepository");
 const favouriteRepository = require("../models/databaseRepositories/favouriteRepository");
 
 /**
