2428b037bc2872c86770a1848e0a8a72b6b2ebfd
houssam966 <houssammahlous123@gmail.com>
2020-03-05 08:24:45 +0000

    add tests

--- server/models/databaseRepositories/signupRepository.test.js
+++ server/models/databaseRepositories/signupRepository.test.js
@@ -38,7 +38,7 @@ test('inserting and finding works', async () => {
     individual.user_id = insertUserResult.rows[0].id;
     const insertIndividualResult = await individualRepository.insert(individual);
 
-    event.address_id =  insertAddressResult.rows[0].id;
+    event.address_id = insertAddressResult.rows[0].id;
     event.user_id = insertUserResult.rows[0].id;
     const insertEventResult = await eventRepository.insert(event);
 
@@ -63,7 +63,7 @@ test('updating works', async () => {
     individual.user_id = insertUserResult.rows[0].id;
     const insertIndividualResult = await individualRepository.insert(individual);
 
-    event.address_id =  insertAddressResult.rows[0].id;
+    event.address_id = insertAddressResult.rows[0].id;
     event.user_id = insertUserResult.rows[0].id;
     const insertEventResult = await eventRepository.insert(event);
 
@@ -75,5 +75,25 @@ test('updating works', async () => {
     const updateSignupResult = await signupRepository.update(signUp);
 
     const findResult = await signupRepository.find(insertIndividualResult.rows[0].id, insertEventResult.rows[0].id);
+    expect(findResult.rows[0]).toMatchObject(signUp);
+});
+test('inserting and finding users signed up to an event works', async () => {
+    const insertRegistrationRepository = await registrationRepository.insert(registration);
+    user.email = insertRegistrationRepository.rows[0].email;
+    const insertUserResult = await userRepository.insert(user);
+    const insertAddressResult = await addressRepository.insert(address);
+    individual.address_id = insertAddressResult.rows[0].id;
+    individual.user_id = insertUserResult.rows[0].id;
+    const insertIndividualResult = await individualRepository.insert(individual);
+
+    event.address_id = insertAddressResult.rows[0].id;
+    event.user_id = insertUserResult.rows[0].id;
+    const insertEventResult = await eventRepository.insert(event);
+
+    signUp.event_id = insertEventResult.rows[0].id;
+    signUp.individual_id = insertIndividualResult.rows[0].id;
+    const insertSignupResult = await signupRepository.insert(signUp);
+
+    const findResult = await signupRepository.findUsersSignedUp(insertEventResult.rows[0].id);
     expect(findResult.rows[0]).toMatchObject(signUp);
 });
--- server/routes/eventSignup.test.js
+++ server/routes/eventSignup.test.js
@@ -1,12 +1,13 @@
 const request = require('supertest');
 const app = require('../app');
 const testHelpers = require("../test/testHelpers");
-
+const util = require("../util/util");
 const signupRepository = require("../models/databaseRepositories/signupRepository");
 const eventRepository = require("../models/databaseRepositories/eventRepository");
 
 jest.mock("../models/databaseRepositories/eventRepository");
 jest.mock("../models/databaseRepositories/signupRepository");
+jest.mock("../util/util");
 
 beforeEach(() => {
     return testHelpers.clearDatabase();
@@ -19,6 +20,8 @@ afterEach(() => {
 
 const signUp = testHelpers.signUp;
 const event = testHelpers.event;
+const signedUpUser1 = testHelpers.signedUpUser1;
+const signedUpUser2 = testHelpers.signedUpUser2;
 event.organization_id = 1;
 event.address_id = 1;
 
@@ -53,9 +56,60 @@ test('requesting event history works', async () => {
     signupRepository.findAllByIndividualId.mockResolvedValue({
         rows: [{}, {}, {}], // 3 events
     });
-    const response = await request(app).get("/event/signUp/history").send({individual_id: 5});
+    const response = await request(app).get("/event/signUp/history").send({
+        individual_id: 5
+    });
 
     expect(signupRepository.findAllByIndividualId).toHaveBeenCalledTimes(1);
     expect(eventRepository.findById).toHaveBeenCalledTimes(3);
     expect(response.statusCode).toBe(200);
+});
+
+test('requesting users signed up to an event works', async () => {
+    util.checkEventId.mockResolvedValue({
+        status: 200
+    });
+    signupRepository.findUsersSignedUp.mockResolvedValue({
+        rows: [{
+            signedUpUser1,
+            signedUpUser2
+        }], // 2 users
+    });
+    const response = await request(app).get("/event/1/signUp");
+
+    expect(signupRepository.findUsersSignedUp).toHaveBeenCalledTimes(1);
+    expect(util.checkEventId).toHaveBeenCalledTimes(1);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toMatchObject([{
+        signedUpUser1,
+        signedUpUser2
+    }]);
+});
+
+test('requesting users signed up to an event that doesnt exist returns event does not exist response', async () => {
+    util.checkEventId.mockResolvedValue({
+        status: 404,
+        message: "No event with specified id",
+    });
+
+    const response = await request(app).get("/event/1/signUp");
+
+    expect(signupRepository.findUsersSignedUp).toHaveBeenCalledTimes(0);
+    expect(util.checkEventId).toHaveBeenCalledTimes(1);
+    expect(response.statusCode).toBe(404);
+    expect(response.text).toBe("No event with specified id");
+});
+
+test('requesting users signed up to an event with wrong id format returns id format is wrong response', async () => {
+    util.checkEventId.mockResolvedValue({
+        status: 400,
+        message: "ID specified is in wrong format",
+    });
+
+    const response = await request(app).get("/event/hhh/signUp");
+
+    expect(signupRepository.findUsersSignedUp).toHaveBeenCalledTimes(0);
+    expect(util.checkEventId).toHaveBeenCalledTimes(1);
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toBe("ID specified is in wrong format");
 });
