b8c8f0762b3232bc4d16305566ef15dda104e0f9
“Mariam <mariam.ahmed@kcl.ac.uk>
2020-03-08 13:01:25 +0000

    Add emailing specific user, removing requests and adding attendees buttons

--- app/Karma/src/components/activities/AttendeeButton.js
+++ app/Karma/src/components/activities/AttendeeButton.js
@@ -5,19 +5,39 @@ import {TouchableOpacity, View} from "react-native";
 import Styles from "../../styles/Styles";
 import {RegularText} from "../text";
 import Colours from "../../styles/Colours";
+import ProfileScreen from "../../views/ProfileScreen"
+import { Icon } from 'react-native-elements'
+import Communications from "react-native-communications";
 
 export default class AttendeeButton extends React.Component {
     render() {
         const {user} = this.props;
         return (
-            <View style={Styles.pv8}>
-                <TouchableOpacity
-                style={[Styles.pv8, {backgroundColor:Colours.white, borderWidth: 3, borderColor: Colours.grey}]}
+            <View style={[Styles.pv8]}>
+                <View
+                style={[Styles.pv8, {flexDirection: "row", paddingRight:20, justifyContent: "space-between", backgroundColor:Colours.white, borderWidth: 3, borderColor: Colours.grey}]}
                 activeOpacity={0.9}>
-                    <RegularText style={[Styles.ph8, {fontSize:20}]}>
-                        {user}
-                    </RegularText>
-                </TouchableOpacity>
+                    <TouchableOpacity>
+                        <RegularText style={[Styles.ph8, {fontSize:20}]}>
+                            {user}
+                        </RegularText>
+
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Icon
+                            name="email"
+                            color={Colours.blue}
+                            onPress={() =>
+                                Communications.email(
+                                    ["userEmail"],
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                )}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
--- app/Karma/src/components/activities/SignUpRequest.js
+++ app/Karma/src/components/activities/SignUpRequest.js
@@ -5,19 +5,40 @@ import {TouchableOpacity, View} from "react-native";
 import Styles from "../../styles/Styles";
 import {RegularText} from "../text";
 import Colours from "../../styles/Colours";
+import { Icon } from 'react-native-elements'
 
 export default class SignUpRequest extends React.Component {
     render() {
         const {user} = this.props;
         return (
-            <View style={Styles.pv8}>
-                <TouchableOpacity
-                style={[Styles.pv8, {backgroundColor:Colours.white, borderWidth: 3, borderColor: Colours.grey}]}
+            <View style={[Styles.pv8]}>
+                <View
+                style={[Styles.pv8, {flexDirection: "row", paddingRight:20, justifyContent: "space-between", backgroundColor:Colours.white, borderWidth: 3, borderColor: Colours.grey}]}
                 activeOpacity={0.9}>
-                    <RegularText style={[Styles.ph8, {fontSize:20}]}>
-                        {user}
-                    </RegularText>
-                </TouchableOpacity>
+                    <TouchableOpacity>
+                        <RegularText style={[Styles.ph8, {fontSize:20}]}>
+                            {user}
+                        </RegularText>
+
+                    </TouchableOpacity>
+                    <View style={{flexDirection:"row", width: 80, justifyContent: "space-between"}}>
+                        <TouchableOpacity>
+                            <Icon
+                                type="entypo"
+                                name="check"
+                                color="#62CD77"
+                                // onPress={() => APPROVE}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Icon
+                                name="close"
+                                color="#E62A2A"
+                                // onPress={() => DISAPPROVE}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         );
     }
