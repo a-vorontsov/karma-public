71fd014603358ccc493b9b066ca81b77baf56d5a
Sten Laane <sten.laane@kcl.ac.uk>
2020-03-08 11:54:34 +0000

    Add and configure pg-camelcase dependency

--- server/routes/event/event.js
+++ server/routes/event/event.js
@@ -17,14 +17,14 @@ const validation = require("../../modules/validation");
 
 /**
  * Endpoint called whenever a user creates a new event.<br/>
- * If an existing address_id is specified in the request, it is reused and no new address is created.<br/>
+ * If an existing addressId is specified in the request, it is reused and no new address is created.<br/>
  * URL example: POST http://localhost:8000/event/
  * @param {Event} req.body - Information regarding the event containing the same properties as this example:
  <pre>
 {
     "address": {
-        "address_1": "Line 1",
-        "address_2": "Line 2",
+        "address1": "Line 1",
+        "address2": "Line 2",
         "postcode": "14 aa",
         "city": "LDN",
         "region": "LDN again",
@@ -32,21 +32,21 @@ const validation = require("../../modules/validation");
         "long": 100.50
     },
     "name": "event",
-    "women_only": "true",
+    "womenOnly": true,
     "spots": 3,
-    "address_visible": "true",
-    "minimum_age": 16,
-    "photo_id": "true",
-    "physical": "true",
-    "add_info": "true",
+    "addressVisible": true,
+    "minimumAge": 16,
+    "photoId": true,
+    "physical": true,
+    "addInfo": true,
     "content": "fun event yay",
     "date": "2004-10-19 10:23:54",
-    "user_id": 3
+    "userId": 3
  }
  </pre>
- * "address" can be substituted with "address_id: {Integer}" in which case the existing address is reused.
+ * "address" can be substituted with "addressId: {Integer}" in which case the existing address is reused.
  * @returns
- *  status: 200, description: The event object created with it's id and address_id set to the ones stored in the database<br/>
+ *  status: 200, description: The event object created with it's id and addressId set to the ones stored in the database<br/>
  *  status: 400, description: User has reached their monthly event creation limit.<br/>
  *  status: 500, description: DB error
  *  @name Create new event
@@ -61,10 +61,10 @@ router.post("/", async (req, res) => {
             return;
         }
 
-        const isIndividual = await util.isIndividual(event.user_id);
+        const isIndividual = await util.isIndividual(event.userId);
         if (isIndividual) {
             const existingUserEvents = await eventRepository.findAllByUserId(
-                event.user_id,
+                event.userId,
             );
             if (existingUserEvents.rows.length >= 3) {
                 return res
@@ -75,13 +75,13 @@ router.post("/", async (req, res) => {
             }
         }
 
-        if (!req.body.address_id) {
+        if (!req.body.addressId) {
             // address doesn't exist in database yet
             const addressResult = await addressRepository.insert(event.address);
-            event.address_id = addressResult.rows[0].id;
+            event.addressId = addressResult.rows[0].id;
         }
 
-        event.creation_date = new Date();
+        event.creationDate = new Date();
         const eventResult = await eventRepository.insert(event);
         res.status(200).send(eventResult.rows[0]);
     } catch (e) {
@@ -98,8 +98,8 @@ router.post("/", async (req, res) => {
 {
     "address": {
         "id": 5,
-        "address_1": "Line 1",
-        "address_2": "Line 2",
+        "address1": "Line 1",
+        "address2": "Line 2",
         "postcode": "14 aa",
         "city": "LDN",
         "region": "LDN again",
@@ -107,16 +107,16 @@ router.post("/", async (req, res) => {
         "long": 100.50
     },
     "name": "event",
-    "women_only": "true",
+    "womenOnly": true,
     "spots": 3,
-    "address_visible": "true",
-    "minimum_age": 16,
-    "photo_id": "true",
-    "physical": "true",
-    "add_info": "true",
+    "addressVisible": true,
+    "minimumAge": 16,
+    "photoId": true,
+    "physical": true,
+    "addInfo": true,
     "content": "fun event yay",
     "date": "2004-10-19 10:23:54",
-    "user_id": 3
+    "userId": 3
  }
  </pre>
  * Note that address must have an id.
@@ -135,7 +135,7 @@ router.post("/update/:id", (req, res) => {
         return;
     }
 
-    event.address_id = address.id;
+    event.addressId = address.id;
     event.id = req.params.id;
     addressRepository
         .update(address)
@@ -147,9 +147,9 @@ router.post("/update/:id", (req, res) => {
 
 /**
  * Endpoint called when "All" tab is pressed in Activities homepage<br/>
- * URL example: http://localhost:8000/event?userId=1&currentPage=1&pageSize=2&filter[]=!women_only&filter[]=physical<br/>
+ * URL example: http://localhost:8000/event?userId=1&currentPage=1&pageSize=2&filter[]=!womenOnly&filter[]=physical<br/>
  * route {GET} /event
- * @param {Integer} req.query.userId - ID of user logged in
+ * @param {Number} req.query.userId - ID of user logged in
  * @param {Array} req.query.filter - all filters required as an array of strings
  * @returns {Object}
  *  status: 200, description: Array of all event objects sorted by time
@@ -164,20 +164,20 @@ router.post("/update/:id", (req, res) => {
    },
    "data": [
        {
-           "event_id": 1,
+           "eventId": 1,
            "name": "Community help centre",
-           "women_only": false,
+           "womenOnly": false,
            "spots": 3,
-           "address_visible": true,
-           "minimum_age": 18,
-           "photo_id": false,
+           "addressVisible": true,
+           "minimumAge": 18,
+           "photoId": false,
            "physical": false,
-           "add_info": true,
+           "addInfo": true,
            "content": "help people at the community help centre because help is good",
            "date": "2020-03-25T19:10:00.000Z",
-           "event_creator_id": 1,
-           "address_1": "nearby road",
-           "address_2": null,
+           "eventCreatorId": 1,
+           "address1": "nearby road",
+           "address2": null,
            "postcode": "whatever",
            "city": "London",
            "region": null,
@@ -186,20 +186,20 @@ router.post("/update/:id", (req, res) => {
             "distance": 0.18548890708299523
        },
        {
-           "event_id": 2,
+           "eventId": 2,
            "name": "Picking up trash",
-           "women_only": false,
+           "womenOnly": false,
            "spots": 5,
-           "address_visible": true,
-           "minimum_age": 18,
-           "photo_id": false,
+           "addressVisible": true,
+           "minimumAge": 18,
+           "photoId": false,
            "physical": false,
-           "add_info": true,
+           "addInfo": true,
            "content": "small class to teach other people how to pick themselves up",
            "date": "2020-03-25T19:10:00.000Z",
-           "event_creator_id": 1,
-           "address_1": "uni road",
-           "address_2": null,
+           "eventCreatorId": 1,
+           "address1": "uni road",
+           "address2": null,
            "postcode": "whatever",
            "city": "London",
            "region": null,
@@ -239,7 +239,7 @@ router.get("/", async (req, res) => {
 /**
  * Endpoint called when "Causes" tab is pressed in Activities homepage<br/>
  * route {GET} event/causes
- * URL example: http://localhost:8000/event/causes?userId=1&filter[]=!women_only&filter[]=physical
+ * URL example: http://localhost:8000/event/causes?userId=1&filter[]=!womenOnly&filter[]=physical
  * @param {Integer} req.query.userId - ID of user logged in
  * @param {Array} req.query.filter - all filters required as an array of strings
  * @returns {any}
@@ -251,21 +251,21 @@ router.get("/", async (req, res) => {
         {
             "id": 3,
             "name": "Staying at Home",
-            "address_id": 1,
-            "women_only": false,
+            "addressId": 1,
+            "womenOnly": false,
             "spots": 1,
-            "address_visible": true,
-            "minimum_age": 18,
-            "photo_id": false,
-            "add_info": false,
+            "addressVisible": true,
+            "minimumAge": 18,
+            "photoId": false,
+            "addInfo": false,
             "content": "sleeping at home",
             "date": "2020-03-25T19:10:00.000Z",
-            "cause_id": 3,
-            "cause_name": "peace",
-            "cause_description": "not dealing with people",
-            "event_creator_id": 1,
-            "address_1": "pincot road",
-            "address_2": null,
+            "causeId": 3,
+            "causeName": "peace",
+            "causeDescription": "not dealing with people",
+            "eventCreatorId": 1,
+            "address1": "pincot road",
+            "address2": null,
             "postcode": "SW19 2LF",
             "city": "London",
             "region": null,
@@ -278,21 +278,21 @@ router.get("/", async (req, res) => {
         {
             "id": 1,
             "name": "Close to Home",
-            "address_id": 3,
-            "women_only": false,
+            "addressId": 3,
+            "womenOnly": false,
             "spots": 3,
-            "address_visible": true,
-            "minimum_age": 18,
-            "photo_id": false,
-            "add_info": false,
+            "addressVisible": true,
+            "minimumAge": 18,
+            "photoId": false,
+            "addInfo": false,
             "content": "very very close from home",
             "date": "2020-03-25T19:10:00.000Z",
-            "cause_id": 1,
-            "cause_name": "gardening",
-            "cause_description": "watering plants and dat",
-            "event_creator_id": 1,
-            "address_1": "nearby road",
-            "address_2": null,
+            "causeId": 1,
+            "causeName": "gardening",
+            "causeDescription": "watering plants and dat",
+            "eventCreatorId": 1,
+            "address1": "nearby road",
+            "address2": null,
             "postcode": "whatever",
             "city": "London",
             "region": null,
@@ -406,21 +406,21 @@ router.get("/going", async (req, res) => {
  {
     "id": 7,
     "name": "event",
-    "address_id": 24,
-    "women_only": true,
+    "addressId": 24,
+    "womenOnly": true,
     "spots": 3,
-    "address_visible": true,
-    "minimum_age": 16,
-    "photo_id": true,
+    "addressVisible": true,
+    "minimumAge": 16,
+    "photoId": true,
     "physical": true,
-    "add_info": true,
+    "addInfo": true,
     "content": "fun event yay",
     "date": "2004-10-19T09:23:54.000Z",
-    "user_id": 27,
+    "userId": 27,
     "address": {
         "id": 24,
-        "address_1": "221B Baker St",
-        "address_2": "Marleybone",
+        "address1": "221B Baker St",
+        "address2": "Marleybone",
         "postcode": "NW1 6XE",
         "city": "London",
         "region": "Greater London",
@@ -439,7 +439,7 @@ router.get("/:id", async (req, res) => {
 
         const eventResult = await eventRepository.findById(id);
         const event = eventResult.rows[0];
-        const addressResult = await addressRepository.findById(event.address_id);
+        const addressResult = await addressRepository.findById(event.addressId);
         const address = addressResult.rows[0];
         res.status(200).send({
             ...event,
