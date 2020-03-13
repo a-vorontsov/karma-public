5647c85e61de528b37f11359cb586b4084be691b
houssam966 <houssammahlous123@gmail.com>
2020-03-13 13:14:07 +0000

    move select causes route to correct directory

--- server/routes/causes/select/index.js
+++ server/routes/causes/select/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const selectedCauseRepository = require("../../models/databaseRepositories/selectedCauseRepository");
+const selectedCauseRepository = require("../../../models/databaseRepositories/selectedCauseRepository");
 
 /**
  * gets called when user selects causes
