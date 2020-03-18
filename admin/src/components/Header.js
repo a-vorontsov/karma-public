7a363f34f31874ca4e640ab1e7314b1bbe60aa41
Sten Laane <sten.laane@kcl.ac.uk>
2020-03-18 18:21:41 +0000

    Use environment variable for image location base in admin site
    Signed-off-by: Sten Laane <sten.laane@kcl.ac.uk>

--- admin/src/components/Header.js
+++ admin/src/components/Header.js
@@ -4,7 +4,7 @@ export class Header extends React.Component {
     render() {
         return (
             <header>
-                <img className={"header-title"} src={"header-title.png"} alt={"Karma"}/>
+                <img className={"header-title"} src={process.env.PUBLIC_URL + "/header-title.png"} alt={"Karma"}/>
             </header>
         );
     }
