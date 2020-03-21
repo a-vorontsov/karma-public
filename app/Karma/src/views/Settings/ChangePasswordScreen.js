67337f0c59b54ee498d96b13667d2eedceb06588
pcscutaru <petrascutaru@Hotmail.com>
2020-03-21 20:19:39 +0000

    Integrate change password in settings

--- app/Karma/src/views/Settings/ChangePasswordScreen.js
+++ app/Karma/src/views/Settings/ChangePasswordScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {SafeAreaView, View, Dimensions} from "react-native";
+import {SafeAreaView, View, Dimensions, Alert} from "react-native";
 import {ScrollView} from "react-native-gesture-handler";
 import SettingsButton from "../../components/buttons/SettingsButton";
 import PageHeader from "../../components/PageHeader";
@@ -8,6 +8,9 @@ import {TextInput} from "../../components/input";
 import ChangePasswordInput from "../../components/input/ChangePasswordInput";
 import {GradientButton} from "../../components/buttons";
 import {RegularText, SubTitleText} from "../../components/text";
+import { getData } from "../../util/credentials";
+
+const request = require("superagent");
 
 const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get("window");
 const FORM_WIDTH = 0.8 * SCREEN_WIDTH;
@@ -17,12 +20,45 @@ export default class ChangePasswordScreen extends Component {
         super(props);
         this.state = {
             isFirstOpened: true,
+            oldPasswordInput: "",
+            passwordInput: "",
         };
     }
     onChange = event => {
         const {name, text} = event;
         this.setState({[name]: text});
     };
+
+    sendNewPass = async () => {
+        const {navigate} = this.props.navigation;
+        console.log("owo" +this.state.oldPasswordInput);
+        this.setState({isFirstOpened: false});
+        const credentials = await getData();
+        await request
+            .post("http://localhost:8000/profile/edit/password")
+            .send({
+                    userId: credentials.username,
+                    oldPassword: this.state.oldPasswordInput,
+                    newPassword: this.state.passwordInput,
+                    confirmPassword:this.state.passwordInput
+                
+            })
+            .then(res => {
+                    Alert.alert("Successful password change", "You've successfully changed your password!", [
+                        {text: "OK", onPress: () => navigate("SettingsMenu")},
+                    ]);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    onInputChange = inputState => {
+        this.setState({
+            passwordInput: inputState.confPassword.confPassword,
+        });
+    };
+
     render() {
         return (
             <SafeAreaView style={[Styles.container, Styles.ph24]}>
@@ -36,6 +72,7 @@ export default class ChangePasswordScreen extends Component {
                             Enter your old password:
                         </SubTitleText>
                         <TextInput
+                            name="oldPasswordInput"
                             placeholder="Verify Old Password"
                             onChange={this.onChange}
                             secureTextEntry={true}
@@ -45,21 +82,23 @@ export default class ChangePasswordScreen extends Component {
                         </SubTitleText>
 
                         <ChangePasswordInput
+                            onChange={this.onInputChange}
                             firstOpen={this.state.isFirstOpened}
                         />
                     </View>
-                </View>
-                <View
+                    <View
                     style={{
                         width: FORM_WIDTH,
                         justifyContent: "flex-end",
                         marginBottom: 40,
                     }}>
                     <GradientButton
-                        onPress={() => this.setState({isFirstOpened: false})}
+                        onPress={() => this.sendNewPass() }
                         title="Change"
                     />
                 </View>
+                </View>
+                
             </SafeAreaView>
         );
     }
