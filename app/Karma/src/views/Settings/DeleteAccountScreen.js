9fd41c5e621997f3543b97f572981f2fb0ab0c3b
pcscutaru <petrascutaru@Hotmail.com>
2020-04-07 18:52:24 +0100

    Add delete account button integration
    *Deleting the account doesn't work properly because the isEmailVerified flag is not set to false*

--- app/Karma/src/views/Settings/DeleteAccountScreen.js
+++ app/Karma/src/views/Settings/DeleteAccountScreen.js
@@ -1,11 +1,49 @@
 import React, {Component} from "react";
-import {SafeAreaView, View} from "react-native";
+import {SafeAreaView, View, Alert} from "react-native";
 import PageHeader from "../../components/PageHeader";
 import Styles from "../../styles/Styles";
 import {RegularText, SemiBoldText} from "../../components/text";
 import {GradientButton, TransparentButton} from "../../components/buttons";
+import {getAuthToken} from "../../util/credentials";
+const request = require("superagent");
+import {REACT_APP_API_URL} from "react-native-dotenv";
+import AsyncStorage from "@react-native-community/async-storage";
 
 class DeleteAccountScreen extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    deleteAccount = async () => {
+        const {navigate} = this.props.navigation;
+        this.setState({isFirstOpened: false});
+        const authToken = await getAuthToken();
+        await request
+            .post(`${REACT_APP_API_URL}/profile/delete`)
+            .set("authorization", authToken)
+            .send({})
+            .then(res => {
+                AsyncStorage.getAllKeys()
+                    .then(keys => AsyncStorage.multiRemove(keys))
+                    .then(() => console.log("owo !!!"));
+                Alert.alert(
+                    "Successful account deletion",
+                    "We've successfully deleted your account.",
+
+                    [{text: "OK", onPress: () => navigate("Welcome")}],
+                );
+            })
+            .catch(err => {
+                Alert.alert(
+                    "Unsuccessful account deletion",
+                    "We didn't manage to detele your account, please try again.",
+
+                    [{text: "OK", onPress: () => navigate("Settings")}],
+                );
+                console.log(err);
+            });
+    };
+
     render() {
         const {navigate} = this.props.navigation;
         return (
@@ -47,7 +85,10 @@ class DeleteAccountScreen extends Component {
                             },
                         ]}>
                         <View style={{marginRight: 20}}>
-                            <GradientButton title="Confirm" />
+                            <GradientButton
+                                title="Confirm"
+                                onPress={() => this.deleteAccount()}
+                            />
                         </View>
                         <View style={{marginLeft: 20}}>
                             <TransparentButton
