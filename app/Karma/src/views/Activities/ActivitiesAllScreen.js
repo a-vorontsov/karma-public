2beed578014a65c836173d8a78a9942e30d11ae0
houssam966 <houssammahlous123@gmail.com>
2020-04-09 22:48:10 +0100

    hide load more button and display text when no more activites

--- app/Karma/src/views/Activities/ActivitiesAllScreen.js
+++ app/Karma/src/views/Activities/ActivitiesAllScreen.js
@@ -14,6 +14,7 @@ import {GradientButton} from "../../components/buttons";
 import Styles from "../../styles/Styles";
 import {getAuthToken} from "../../util/credentials";
 import {REACT_APP_API_URL} from "react-native-dotenv";
+import Colours from "../../styles/Colours";
 const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get("window");
 const queryString = require("query-string");
 const formWidth = 0.6 * SCREEN_WIDTH;
@@ -29,6 +30,7 @@ class ActivitiesAllScreen extends Component {
             loading: true, // Loading state used while loading the data for the first time
             activitiesList: [], // Data Source for the FlatList
             fetchingDataFromServer: false, // Loading state used while loading more data
+            allEventsFetched: false,
         };
         this.page = 1;
         this.fetchActivities = this.fetchActivities.bind(this);
@@ -92,7 +94,6 @@ class ActivitiesAllScreen extends Component {
                     ? filters.push("physical")
                     : filters.push("!physical");
             }
-            console.log(filters);
             return queryString.stringify(
                 {filter: filters},
                 {arrayFormat: "bracket", encode: false},
@@ -113,16 +114,24 @@ class ActivitiesAllScreen extends Component {
                 ...this.getFiltersObject(),
             })
             .then(async res => {
-                this.page = this.page + 1; //Increasing the offset for the next API call.
-
-                this.setState({
-                    loading: false,
-                    fetchingDataFromServer: false,
-                    activitiesList: [
-                        ...this.state.activitiesList,
-                        ...res.body.data.events,
-                    ], //adding the new data with old one available in Data Source of the List
-                });
+                if (res.body.data.meta.pageSize === 0) {
+                    this.setState({
+                        allEventsFetched: true,
+                        loading: false,
+                        fetchingDataFromServer: false,
+                    });
+                } else {
+                    this.page = this.page + 1; //Increasing the offset for the next API call.
+                    console.log(res.body.data.meta);
+                    this.setState({
+                        loading: false,
+                        fetchingDataFromServer: false,
+                        activitiesList: [
+                            ...this.state.activitiesList,
+                            ...res.body.data.events,
+                        ], //adding the new data with old one available in Data Source of the List
+                    });
+                }
             })
             .catch(er => {
                 console.log(er);
@@ -136,14 +145,23 @@ class ActivitiesAllScreen extends Component {
         return (
             //Footer View with Load More button
             <View style={{width: formWidth, marginLeft: formWidth * 0.3}}>
-                {this.state.fetchingDataFromServer ? (
+                {this.state.fetchingDataFromServer && (
                     <ActivityIndicator color="grey" style={{marginLeft: 8}} />
-                ) : (
-                    <GradientButton
-                        onPress={this.fetchActivities}
-                        title="Load More"
-                    />
                 )}
+                {this.state.allEventsFetched && (
+                    <View style={{alignItems: "center"}}>
+                        <RegularText style={{fontSize: 18, color: Colours.lightBlue, alignItems:"center"}}>
+                            There aren't any more activities!
+                        </RegularText>
+                    </View>
+                )}
+                {!this.state.fetchingDataFromServer &&
+                    !this.state.allEventsFetched && (
+                        <GradientButton
+                            onPress={this.fetchActivities}
+                            title="Load More"
+                        />
+                    )}
             </View>
         );
     }
@@ -166,6 +184,7 @@ class ActivitiesAllScreen extends Component {
                 this.setState({
                     isRefreshing: false,
                     activitiesList: res.body.data.events,
+                    allEventsFetched: false,
                 });
             })
             .catch(er => {
