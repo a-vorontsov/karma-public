7d2ad6ac87afcb57eee7c764857576e83cebd21a
Sten Laane <sten.laane@kcl.ac.uk>
2020-04-13 15:42:04 +0100

    Add useful links to admin page

--- web/src/components/TabView.js
+++ web/src/components/TabView.js
@@ -42,13 +42,21 @@ export function TabView() {
         <div>
             <AppBar position="static">
                 <Tabs value={value} onChange={handleChange} className={"tabView"}>
-                    <Tab label="Statistics" {...a11yProps(0)} />
+                    <Tab label="Links" {...a11yProps(0)} />
                     <Tab label="User management" {...a11yProps(1)} />
                     <Tab label="T&C and Privacy Policy" {...a11yProps(2)} />
                 </Tabs>
             </AppBar>
             <TabPanel value={value} index={0}>
-                <p>Cool graphs and stuff go here</p>
+                <ul className={'links'}>
+                    <li>Android Application - <a href={'https://github.com/a-vorontsov/karma-public/releases/download/1.0.0/KARMA.apk'}>https://github.com/a-vorontsov/karma-public/releases/download/1.0.0/KARMA.apk</a></li>
+                    <li>iOS Application - <a href={'https://testflight.apple.com/join/2mDl8SbS'}>https://testflight.apple.com/join/2mDl8SbS</a></li>
+                    <li>KARMA Administrator Website - <a href={'https://karma.laane.xyz/admin'}>https://karma.laane.xyz/admin</a></li>
+                    <li>API - <a href={'https://karma.laane.xyz/api'}>https://karma.laane.xyz/api</a></li>
+                    <li>Documentation - <a href={'https://karma.laane.xyz/docs'}>https://karma.laane.xyz/docs</a></li>
+                    <li>Logs - <a href={'https://karma.laane.xyz/logs'}>https://karma.laane.xyz/logs</a></li>
+                    <li>Test coverage - <a href={'https://karma.laane.xyz/coverage'}>https://karma.laane.xyz/coverage</a></li>
+                </ul>
             </TabPanel>
             <TabPanel value={value} index={1}>
                 <IndividualsTableTab/>
